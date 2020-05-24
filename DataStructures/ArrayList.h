4ab9050504c8746163ee37a2fbd43ae87ffb035d
Mani9732 <mani.shah23@gmail.com>
2020-05-24 14:49:59 -0400

    Added a few function

--- DataStructures/ArrayList.h
+++ DataStructures/ArrayList.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <string>
 
 using namespace std;
 
@@ -10,6 +11,11 @@ private:
 	int length_of_storage;
 	int num_of_elements;
 	int* storage;
+
+
+	int* grow_array(bool halve);
+	void validate_range(int index);
+
 
 public:
 	ArrayList() {
@@ -31,15 +37,117 @@ public:
 	int capacity();
 	int set(int index, int value);
 	int get(int index);
-	int add(int value);
-	int add(int index, int value);
+	bool add(int value);
+	void add(int index, int value);
 	int remove(int index);
-
-
-
-
-
-
-
-
+	string toString();
 };
+
+void ArrayList::validate_range(int index)
+{
+	if (index < 0 || index > this->length_of_storage) {
+		throw "Invalid Index";
+	}
+}
+
+int* ArrayList::grow_array(bool halve)
+{
+	if (halve) {
+		this->length_of_storage = this->length_of_storage >> 1;
+	}
+	else {
+		this->length_of_storage = this->length_of_storage << 1;
+	}
+	int* temp = new int[this->length_of_storage];
+
+	for (int i = 0; i < this->length_of_storage; i++) {
+		temp[i] = this->storage[i];
+	}
+
+	delete[] this->storage;
+
+	return temp;
+}
+
+int ArrayList::size()
+{
+	return this->num_of_elements;
+}
+
+int ArrayList::capacity()
+{
+	return this->length_of_storage;
+}
+
+int ArrayList::set(int index, int value)
+{
+	validate_range(index);
+	int oldItem = storage[index];
+	storage[index] = value;
+	return oldItem;
+}
+
+int ArrayList::get(int index)
+{
+	validate_range(index);
+	return storage[index];
+}
+
+bool ArrayList::add(int value)
+{
+	if (this->num_of_elements == this->length_of_storage)
+	{
+		this->storage = grow_array(false);
+	}
+	this->storage[this->num_of_elements] = value;
+	this->num_of_elements++;
+
+	return true;
+}
+
+void ArrayList::add(int index, int value)
+{
+	validate_range(index);
+
+	if (this->num_of_elements == this->length_of_storage) {
+		this->storage = grow_array(false);
+	}
+	if (index == this->num_of_elements) {
+		add(value);
+	}
+	else {
+		int end = this->num_of_elements - 1;
+		for (int i = end; i >= index; i--) {
+			this->storage[i + 1] = this->storage[i];
+		}
+		this->storage[index] = value;
+		++this->num_of_elements;
+	}
+}
+
+int ArrayList::remove(int index)
+{
+	validate_range(index);
+	int oldItem = this->storage[index];
+
+	if (this->num_of_elements < ((1 / 3) * this->length_of_storage)) {
+		this->storage = grow_array(true);
+	}
+	if (index == this->num_of_elements - 1) {
+		this->storage[index] = NULL;
+	}
+	else {
+		int end = this->num_of_elements - 1;
+		int currIndex = index + 1;
+		for (int i = currIndex; i < index; i++) {
+			this->storage[i - 1] = this->storage[i];
+		}
+		this->storage[end] = NULL;
+		--this->num_of_elements;
+	}
+	return oldItem;
+}
+
+
+
+
--- DataStructures/ArrayListTest.cpp
+++ DataStructures/ArrayListTest.cpp
@@ -1,2 +1,16 @@
 #include "ArrayList.h"
+#include <iostream>
+#include <array>
 
+using namespace std;
+
+int main()
+{
+	
+	std::array<int, 4> A = { 10,20,30,40 };
+	std::array<int, 4> B = A; //copy array A into array B
+
+	cout << B[1];
+
+	return 0;
+}
